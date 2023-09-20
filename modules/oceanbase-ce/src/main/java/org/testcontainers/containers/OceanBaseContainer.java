a4328813050ad73e87c6850bd13bcf69ef8b51dc
He Wang <wanghechn@qq.com>
2023-09-20 15:01:51 +0800

    upgrade container and image

--- modules/oceanbase-ce/src/main/java/org/testcontainers/containers/OceanBaseContainer.java
+++ modules/oceanbase-ce/src/main/java/org/testcontainers/containers/OceanBaseContainer.java
@@ -1,6 +1,8 @@
 package org.testcontainers.containers;
 
+import org.apache.commons.lang3.StringUtils;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.Transferable;
 import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
@@ -28,9 +30,52 @@ public class OceanBaseContainer extends JdbcDatabaseContainer<OceanBaseContainer
 
     private static final Integer RPC_PORT = 2882;
 
-    private final String databaseName = "test";
-    private final String username = "root@test";
-    private final String password = "";
+    private static final String INIT_SCRIPT_PATH = "/root/boot/init.d/";
+    private static final String SYSTEM_TENANT = "sys";
+    private static final String DEFAULT_USERNAME = "root";
+    private static final String DEFAULT_PASSWORD = "";
+    private static final String DEFAULT_TENANT_NAME = "test";
+    private static final String DEFAULT_DATABASE_NAME = "test";
+
+    /**
+     * The deployment mode of OceanBase. See <a href="https://hub.docker.com/r/oceanbase/oceanbase-ce">Docker Hub</a> for more details.
+     */
+    enum Mode {
+        /**
+         * Standard standalone deployment with a monitor service.
+         */
+        NORMAL,
+
+        /**
+         * Similar to 'normal' mode, but uses less hardware resources.
+         */
+        MINI,
+
+        /**
+         * Standalone deployment without the monitor service, which uses the least hardware resources.
+         */
+        SLIM;
+
+        static Mode fromString(String mode) {
+            if (StringUtils.isEmpty(mode)) {
+                throw new IllegalArgumentException("Mode cannot be null or empty");
+            }
+            switch (mode.trim().toLowerCase()) {
+                case "normal":
+                    return NORMAL;
+                case "mini":
+                    return MINI;
+                case "slim":
+                    return SLIM;
+                default:
+                    throw new IllegalArgumentException("Unsupported mode: " + mode);
+            }
+        }
+    }
+
+    private Mode mode = Mode.SLIM;
+    private String sysRootPassword = DEFAULT_PASSWORD;
+    private String tenantName = DEFAULT_TENANT_NAME;
 
     public OceanBaseContainer(String dockerImageName) {
         this(DockerImageName.parse(dockerImageName));
@@ -46,29 +91,46 @@ public class OceanBaseContainer extends JdbcDatabaseContainer<OceanBaseContainer
     }
 
     @Override
+    protected void waitUntilContainerStarted() {
+        getWaitStrategy().waitUntilReady(this);
+    }
+
+    public Integer getSqlPort() {
+        return getActualPort(SQL_PORT);
+    }
+
+    public Integer getActualPort(int port) {
+        return "host".equals(getNetworkMode()) ? port : getMappedPort(port);
+    }
+
+    @Override
     public String getDriverClassName() {
         return "com.mysql.cj.jdbc.Driver";
     }
 
     @Override
     public String getJdbcUrl() {
+        return getJdbcUrl(DEFAULT_DATABASE_NAME);
+    }
+
+    public String getJdbcUrl(String databaseName) {
         String additionalUrlParams = constructUrlParameters("?", "&");
-        return "jdbc:mysql://" + getHost() + ":" + getMappedPort(SQL_PORT) + "/" + databaseName + additionalUrlParams;
+        return "jdbc:mysql://" + getHost() + ":" + getSqlPort() + "/" + databaseName + additionalUrlParams;
     }
 
     @Override
     public String getDatabaseName() {
-        return databaseName;
+        return DEFAULT_DATABASE_NAME;
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return DEFAULT_USERNAME + "@" + tenantName;
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return DEFAULT_PASSWORD;
     }
 
     @Override
@@ -76,8 +138,62 @@ public class OceanBaseContainer extends JdbcDatabaseContainer<OceanBaseContainer
         return "SELECT 1";
     }
 
+    /**
+     * Set the deployment mode.
+     *
+     * @param mode the deployment mode, can be 'slim', 'mini' or 'normal'
+     * @return this
+     */
+    public OceanBaseContainer withMode(String mode) {
+        this.mode = Mode.fromString(mode);
+        return self();
+    }
+
+    /**
+     * Set the root password of sys tenant.
+     *
+     * @param sysRootPassword the root password of sys tenant
+     * @return this
+     */
+    public OceanBaseContainer withSysRootPassword(String sysRootPassword) {
+        if (sysRootPassword == null) {
+            throw new IllegalArgumentException("Password of sys tenant root user cannot be null");
+        }
+        this.sysRootPassword = sysRootPassword;
+        return self();
+    }
+
+    /**
+     * Set the non-sys tenant name to be created for testing.
+     *
+     * @param tenantName the tenant name to be created
+     * @return this
+     */
+    public OceanBaseContainer withTenantName(String tenantName) {
+        if (StringUtils.isEmpty(tenantName)) {
+            throw new IllegalArgumentException("Tenant name cannot be null or empty");
+        }
+        if (SYSTEM_TENANT.equals(tenantName)) {
+            throw new IllegalArgumentException("Tenant name cannot be " + SYSTEM_TENANT);
+        }
+        this.tenantName = tenantName;
+        return self();
+    }
+
+    /**
+     * Set the sql file for initialization, which will be executed in the test tenant.
+     *
+     * @param sqlFile the sql file
+     * @return this
+     */
+    public OceanBaseContainer withInitSqlFile(Transferable sqlFile) {
+        return withCopyToContainer(sqlFile, INIT_SCRIPT_PATH);
+    }
+
     @Override
-    protected void waitUntilContainerStarted() {
-        getWaitStrategy().waitUntilReady(this);
+    protected void configure() {
+        withEnv("MODE", mode.toString().toLowerCase());
+        withEnv("OB_ROOT_PASSWORD", sysRootPassword);
+        withEnv("OB_TENANT_NAME", tenantName);
     }
 }
